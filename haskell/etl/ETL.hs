f04f998b582c62ede268ecc82c0ed3dce03ddea7
Brett Lischalk <blischalk@opinionlab.com>
2015-11-13 17:10:05 -0600

    Make things a little more legible.

--- haskell/etl/ETL.hs
+++ haskell/etl/ETL.hs
@@ -6,13 +6,18 @@ import Data.Char
 type Letter = String
 type Points = Int
 
-flattenOldMap :: [(Points, [Letter])] -> [(Letter, Points)]
-flattenOldMap l = Prelude.foldl (++) [] $ Prelude.map (\(point,letters) -> (Prelude.map (\ch -> (ch,point)) letters)) l
-
 transform :: Map.Map Points [Letter] -> Map.Map Letter Points
 transform oldData = let oldDataMapAsList      = Map.toList oldData
                         formattedForInsertion = flattenOldMap oldDataMapAsList
                     in Map.fromList $ Prelude.map toLowerString formattedForInsertion
+
+flattenOldMap :: [(Points, [Letter])] -> [(Letter, Points)]
+flattenOldMap l = Prelude.foldl (++) [] $ flattenLetters l
+
+flattenLetters :: [(Points, [Letter])] -> [[(Letter, Points)]]
+flattenLetters = Prelude.map (\(point,letters) ->
+                                  (Prelude.map (\ch ->
+                                                (ch,point)) letters))
 
 toLowerString :: (Letter, Points) -> (Letter, Points)
 toLowerString (c,p) = (map toLower c,p)
