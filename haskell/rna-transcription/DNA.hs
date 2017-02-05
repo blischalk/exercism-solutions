ce49a2478dd196cc45a1094a179ac678fa362221
Brett Lischalk <blischalk@opinionlab.com>
2017-02-05 15:49:10 -0600

    Enhancing error handling.
    Using higher order function instead of recursion.

--- haskell/rna-transcription/DNA.hs
+++ haskell/rna-transcription/DNA.hs
@@ -1,10 +1,13 @@
 module DNA (toRNA) where
 
 toRNA :: [Char] -> [Char]
-toRNA [] = []
-toRNA (x:xs) = case x of 'G' -> 'C'
-                         'C' -> 'G'
-                         'T' -> 'A'
-                         'A' -> 'U'
-                         _   -> error "undefined"
-               : toRNA xs
+toRNA xs = 
+  foldr (\x y ->
+      let v = case x of 'G' -> 'C'
+                        'C' -> 'G'
+                        'T' -> 'A'
+                        'A' -> 'U'
+                        _   -> error $ "Undefined transformation encountered for char: " ++ show x
+      in v:y)
+  []
+  xs
