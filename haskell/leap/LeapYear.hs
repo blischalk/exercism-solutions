51b25c62150e9f51980458db70e79140bfd94b15
Brett Lischalk <blischalk@opinionlab.com>
2017-02-05 15:27:38 -0600

    Refining without guards

--- haskell/leap/LeapYear.hs
+++ haskell/leap/LeapYear.hs
@@ -1,8 +1,5 @@
 module LeapYear (isLeapYear) where
 
 isLeapYear :: Int -> Bool
-isLeapYear y 
-  | y `rem` 4 == 0 = if y `rem` 100 == 0 && y `rem` 400 /= 0
-                     then False
-                     else True
-  | otherwise      = False
+isLeapYear y = y `rem` 4 == 0 &&
+               (not $ y `rem` 100 == 0 && y `rem` 400 /= 0)
